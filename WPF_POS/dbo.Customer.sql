ac7a1f1fe748918f093474c1bcd1d3d460a638af
Yohan <90400063+yohanmoon@users.noreply.github.com>
2021-11-05 00:27:11 -0400

    Delete WPF_POS directory

--- WPF_POS/dbo.Customer.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].Customer
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Last Name] NVARCHAR(50) NOT NULL, 
-    [First Name] NVARCHAR(50) NOT NULL, 
-    [Middle Initial] NCHAR(10) NULL, 
-    [Gender] NCHAR(10) NULL, 
-    [Order] INT NULL
-)
